8e9a7240a63a45167a7f89faa84da9b36772f959
Sergio2R <searodriguezr@correo.udistrital.edu.co>
2022-06-06 23:03:15 -0500

    Add project files.

---
+++ NetCoreWepAPI/Controllers/BeerController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NetCoreWepAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BeerController : ControllerBase
+    {
+        List<Beer> Beers = new List<Beer>()
+        {
+            new Beer() { Id= 1, Name = "Corona" }
+        };
+
+        [HttpGet]
+        public ActionResult<Beer> Get(int Id)
+        {
+            var beer = Beers.Where(x => x.Id == Id).FirstOrDefault();
+            if (beer == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return beer;
+            }
+        }
+    }
+
+    public class Beer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
