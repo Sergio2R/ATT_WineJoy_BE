b05cb5a23d2ea2d6dcd42d49d50cb5b5c76607a0
Sergio2R <searodriguezr@correo.udistrital.edu.co>
2022-06-07 07:22:17 -0500

    update model

---
+++ NetCoreWepAPI/Wine.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace NetCoreWepAPI
+{
+    public class Wine
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Year { get; set; }
+        public string Aroma { get; set; }
+        public float Swetness { get; set; }
+        public float Acidity { get; set; }
+        public float Alcohol { get; set; }
+        public string Notes { get; set; }
+
+    }
+}
